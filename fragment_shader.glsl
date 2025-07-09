1883c3a759c583d5f3b48cb87cc745a56a3dd298
Guilherme Rolim <guilherme.rolim520@gmail.com>
2025-07-09 10:13:49 -0300

    Finished recursive raytrace function

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -20,19 +20,24 @@ struct HitInfo {
     vec3 position;
     vec3 normal;
     vec3 color;
+    float reflectivity;
 };
 
-// A sphere is a center, radius, and color
+// A sphere is a center, radius, color and reflectivity
 struct Sphere {
     vec3 center;
     float radius;
     vec3 color;
+    float reflectivity;
 };
 
 // --- Scene Definition ---
 const int NUM_SPHERES = 3;
 Sphere scene[NUM_SPHERES];
 const vec3 BACKGROUND_COLOR = vec3(0.1, 0.15, 0.2); // Corresponds to "Return the background color"
+
+// Forward declaration for the recursive function
+vec3 RayTrace(Ray ray, int depth);
 
 // --- Ray-Sphere Intersection ---
 // Solves the quadratic equation for ray-sphere intersection.
@@ -61,6 +66,7 @@ HitInfo intersect_sphere(Ray ray, Sphere s) {
         info.position = ray.origin + t * ray.direction;
         info.normal = normalize(info.position - s.center);
         info.color = s.color;
+        info.reflectivity = s.reflectivity;
     }
     return info;
 }
@@ -69,21 +75,6 @@ HitInfo intersect_sphere(Ray ray, Sphere s) {
 // --- Main Trace Function ---
 // This corresponds to "Check the ray... let z be the first intersection point"
 HitInfo trace(Ray ray) {
-    HitInfo closest_hit;
-    closest_hit.hit = false;
-    closest_hit.t = 1e30; // A very large number (infinity)
-
-    for (int i = 0; i < NUM_SPHERES; ++i) {
-        HitInfo current_hit = intersect_sphere(ray, scene[i]);
-        if (current_hit.hit && current_hit.t < closest_hit.t) {
-            closest_hit = current_hit;
-        }
-    }
-    return closest_hit;
-}
-
-// This corresponds to "Check the ray... let z be the first intersection point"
-HitInfo verify_intersections(Ray ray) {
     HitInfo closest_hit;
     closest_hit.hit = false;
     closest_hit.t = 1e30; // A very large number (infinity)
@@ -147,29 +138,39 @@ vec3 RayTrace(Ray ray, int depth) {
 
     // Nonrecursive Computations
     // Check if the ray intersects any of the objects in the scene
-    HitInfo hit = verify_intersections(ray);
+    HitInfo hit = trace(ray);
 
     if (!hit.hit) {
-        // If there id no object intersection, we return the background color
+        // If there is no object intersection, we return the background color
         return BACKGROUND_COLOR;
     }
     // If we hit an object, calculate its color using the Phong model
-    vec3 color = phong_lighting(hit, u_light_pos, u_camera_pos);
+    vec3 local_color = phong_lighting(hit, u_light_pos, u_camera_pos);
 
     // Recursive Computations
-    if (depth == 0) {
-        return color; // Reached maximum depth
+    if (depth <= 0 || hit.reflectivity <= 0.0) {
+        return local_color; // Reached maximum depth or not reflective
     }
-    
+
+    // Calculate reflected ray
+    Ray reflected_ray;
+    reflected_ray.origin = hit.position;
+    reflected_ray.direction = reflect(ray.direction, hit.normal);
+
+    // Recursively trace the reflected ray
+    vec3 reflected_color = RayTrace(reflected_ray, depth - 1);
+
+    // Combine local and reflected color
+    return mix(local_color, reflected_color, hit.reflectivity);
 }
 
 
 void main() {
     // --- Setup the Scene ---
     // We define the scene objects here
-    scene[0] = Sphere(vec3(0.0, 0.0, -1.0), 0.5, vec3(1.0, 0.2, 0.2));  // Red sphere
-    scene[1] = Sphere(vec3(-1.2, 0.0, -2.0), 0.5, vec3(0.2, 1.0, 0.2)); // Green sphere
-    scene[2] = Sphere(vec3(1.2, 0.0, -2.0), 0.5, vec3(0.2, 0.2, 1.0));  // Blue sphere
+    scene[0] = Sphere(vec3(0.0, 0.0, -1.0), 0.5, vec3(1.0, 0.2, 0.2), 0.8);  // Red sphere
+    scene[1] = Sphere(vec3(-1.2, 0.0, -2.0), 0.5, vec3(0.2, 1.0, 0.2), 0.2); // Green sphere
+    scene[2] = Sphere(vec3(1.2, 0.0, -2.0), 0.5, vec3(0.2, 0.2, 1.0), 0.2);  // Blue sphere
 
     // --- Primary Ray Generation ---
     // This is the "For each pixel p" part from RayTraceMain()
@@ -177,10 +178,10 @@ void main() {
 
     // The distance from the camera to the virtual viewing plane.
     // A smaller value creates a wider field of view (like a wide-angle lens).
-    const float focal_length = 1;
+    const float focal_length = 1.0;
 
     // Maximum depth of the ray tracing
-    const int max_depth = 5
+    const int max_depth = 5;
 
     Ray primary_ray;
     primary_ray.origin = u_camera_pos;
@@ -188,16 +189,7 @@ void main() {
 
     // --- Trace and Color ---
     // This calls the main RayTrace logic
-    HitInfo hit = trace(primary_ray);
-
-    vec3 final_color;
-    if (hit.hit) {
-        // If we hit an object, calculate its color using the Phong model
-        final_color = phong_lighting(hit, u_light_pos, u_camera_pos);
-    } else {
-        // Otherwise, return the background color
-        final_color = BACKGROUND_COLOR;
-    }
+    vec3 final_color = RayTrace(primary_ray, max_depth);
 
     FragColor = vec4(final_color, 1.0);
 }
